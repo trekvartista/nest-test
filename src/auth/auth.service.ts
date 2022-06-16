2f7d2138c1996584e5b0123dc381da4a6cef86df
trekvartista <isken_150502@mail.ru>
2022-06-17 01:33:18 +0600

    Implemented login logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { CreateUserDto } from "src/users/dto/create-user.dto";
 import { UsersService } from "src/users/users.service";
@@ -12,7 +12,10 @@ export class AuthService {
         private jwtService: JwtService
     ) {}
 
-    async login(userDto: CreateUserDto) {}
+    async login(userDto: CreateUserDto) {
+        const user = await this.validateUser(userDto);
+        return this.generateToken(user);
+    }
 
     async register(userDto: CreateUserDto) {
         const candidate = await this.userService.getUserByEmail(userDto.email);
@@ -25,10 +28,20 @@ export class AuthService {
         return this.generateToken(user);
     }
 
-    async generateToken (user: User) {
+    private async generateToken (user: User) {
         const payload = {email: user.email, id: user.id, roles: user.roles};
         return {
             token: this.jwtService.sign(payload)
         }
+    }
+
+    private async validateUser (userDto: CreateUserDto) {
+        const user = await this.userService.getUserByEmail(userDto.email);
+        const passwordEquals = await bcrypt.compare(userDto.password, user.password);
+
+        if (user && passwordEquals) {
+            return user;
+        }
+        throw new UnauthorizedException({message: 'Incorrect email or password'})
     }
 }
