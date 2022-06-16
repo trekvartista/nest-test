e90db707b8936ca9daeb7fa6106d2d7af46a93a0
trekvartista <isken_150502@mail.ru>
2022-06-16 23:47:43 +0600

    Set up swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from "@nestjs/core"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function start() {
     const PORT = process.env.PORT || 5000
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle("Nest app")
+        .setDescription("REST API Documentation")
+        .setVersion("1.0.0")
+        // .addTag("NestJS")
+        .build()
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api/docs', app, document);
 
     await app.listen(PORT, () => console.log('Server started on PORT, ', PORT));
 }
