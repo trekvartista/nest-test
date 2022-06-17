6ba092332c9a2189f1999a672539bd9487590e09
trekvartista <isken_150502@mail.ru>
2022-06-17 22:13:45 +0600

    Implemented auth guard (use UseGuards() in controller)
    This guard prevents unauthorized users access data that only authorized users can access

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { UsersModule } from "src/users/users.module";
 import { AuthController } from "./auth.controller";
@@ -8,11 +8,16 @@ import { AuthService } from "./auth.service";
     controllers: [AuthController],
     providers: [AuthService],
     imports: [
-        UsersModule,
+        
+		forwardRef(() => UsersModule),	// to prevent circular dependency,
         JwtModule.register({
             secret: process.env.PRIVATE_KEY || "SECRET",
             signOptions: { expiresIn: "24h" }
         })
+    ],
+    exports: [
+        AuthService,
+        JwtModule
     ]
 })
 export class AuthModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { AuthModule } from 'src/auth/auth.module';
 import { Role } from 'src/roles/roles.model';
 import { RolesModule } from 'src/roles/roles.module';
 import { UserRoles } from 'src/roles/user-roles.model';
@@ -12,7 +13,8 @@ import { UsersService } from './users.service';
 	providers: [UsersService],
 	imports: [
     	SequelizeModule.forFeature([User, Role, UserRoles]),
-		RolesModule
+		RolesModule,
+		forwardRef(() => AuthModule)	// to prevent circular dependency
 	],
 	exports: [
 		UsersService
