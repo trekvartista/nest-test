3bbd5edfea1ae107d6bec01ff8ec20789f63b2e4
trekvartista <isken_150502@mail.ru>
2022-06-19 15:02:19 +0600

    created and configured post models

---
+++ src/posts/posts.model.ts
@@ -0,0 +1,41 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { BelongsTo, BelongsToMany, Column, DataType, ForeignKey, Model, Table } from "sequelize-typescript";
+import { User } from "src/users/users.model";
+
+interface PostCreationAttributes {
+    title: string;
+    description: string;
+}
+
+@Table({ tableName: "posts" })
+export class Post extends Model<Post, PostCreationAttributes> {
+
+    @ApiProperty({example: 1, description: 'Post\'s unique identificator'})
+    @Column({
+        type: DataType.INTEGER,
+        unique: true,
+        autoIncrement: true,
+        primaryKey: true
+    })
+    id: number;
+
+    @ApiProperty({example: '2b2t issue', description: 'Title of the post'})
+    @Column({ type: DataType.STRING, allowNull: false })
+    title: string;
+
+    @ApiProperty({example: 'Lorem ipsum ...', description: 'Main content of the post'})
+    @Column({ type: DataType.STRING, allowNull: false })
+    content: string;
+
+    @ApiProperty({example: 'https://media.sproutsocial.com/uploads/2017/01/Instagram-Post-Ideas.png', description: 'Post image Url'})
+    @Column({type: DataType.STRING})
+    imageUrl: string
+
+    @ApiProperty({example: 5, description: 'Unique userId of the Author'})
+    @Column({type: DataType.INTEGER})
+    @ForeignKey(() => User)
+    userId: number;
+
+    @BelongsTo(() => User)
+    author: User
+}
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { BelongsToMany, Column, DataType, Model, Table } from "sequelize-typescript";
+import { BelongsToMany, Column, DataType, HasMany, Model, Table } from "sequelize-typescript";
+import { Post } from "src/posts/posts.model";
 import { Role } from "src/roles/roles.model";
 import { UserRoles } from "src/roles/user-roles.model";
 
@@ -37,4 +38,7 @@ export class User extends Model<User, UserCreationAttributes> {
 
     @BelongsToMany(() => Role, () => UserRoles)
     roles: Role[]
+
+    @HasMany(() => Post)
+    posts: Post[]
 }
