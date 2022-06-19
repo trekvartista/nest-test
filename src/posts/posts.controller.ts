ce3f4d18fd9eac721c71c533b1d7e193690762ee
trekvartista <isken_150502@mail.ru>
2022-06-20 01:49:02 +0600

    Documented remaining endpoints in swagger

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,12 +1,17 @@
 import { Body, Controller, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { CreatePostDto } from "./dto/create-post.dto";
 import { PostsService } from "./posts.service";
+import { Post as UserPost } from "./posts.model";
 
+@ApiTags('Posts')
 @Controller("posts")
 export class PostsController {
     constructor(private postService: PostsService) {}
 
+    @ApiOperation({summary: 'Post creation'})
+    @ApiResponse({status: 200, type: UserPost})
     @Post()
     @UseInterceptors(FileInterceptor('image'))
     createPost(@Body() dto: CreatePostDto, @UploadedFile() image) {
--- src/users/dto/ban-user.dto.ts
+++ src/users/dto/ban-user.dto.ts
@@ -1,4 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString } from "class-validator";
+
 export class BanUserDto {
+    @ApiProperty({example: 1, description: "User\'s unique id"})
+    @IsNumber({}, {message: "Number is expected"})
     readonly userId: number;
+
+    @ApiProperty({example: "Spam", description: "Ban reason. Must be a good reason"})
+    @IsString({message: "String is expected"})
     readonly reason: string;
 }
